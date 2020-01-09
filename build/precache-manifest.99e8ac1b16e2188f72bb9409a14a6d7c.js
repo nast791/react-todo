3058716bba7cd2b06daff8d47b02efde6fd243a5
Anastasiya Vasina <nast791@mail.ru>
2020-01-09 20:43:45 +0300

    build

---
+++ build/precache-manifest.99e8ac1b16e2188f72bb9409a14a6d7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be21f8a2c3bdc3423122f8e29c1fb240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2401fcf2e63311ffbe47",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0171e08f975579499053",
+    "url": "/static/js/2.a6f4b200.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a6f4b200.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2401fcf2e63311ffbe47",
+    "url": "/static/js/main.c5d65bde.chunk.js"
+  },
+  {
+    "revision": "d508612cddbc4b6a5b0d",
+    "url": "/static/js/runtime-main.3948777d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
