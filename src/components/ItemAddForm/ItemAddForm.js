459925b7b5dd0f7cac80452ec4666bf33677df50
Anastasiya Vasina <nast791@mail.ru>
2020-01-12 17:26:05 +0300

    filter, search, additem

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -4,14 +4,32 @@ import './ItemAddForm.scss';
 export class ItemAddForm extends Component {
   constructor() {
     super();
+
+    this.state = {
+      label: ''
+    };
+
+    this.onLabelChange = (e) => {
+      this.setState({
+        label: e.target.value
+      });
+    }
+
+    this.onSubmit = (e) => {
+      e.preventDefault();
+      if (this.state.label) {
+        this.props.addItem(this.state.label);
+        this.setState({label: ''});
+      }
+    }
   }
 
   render() {
-    const {addItem} = this.props;
     return (
-      <div className="item-add-form">
-        <button className="btn btn-outline-secondary float-right" type="button" onClick={() => addItem('Выпить кофе')}>Добавить</button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input type="text" className="form-control" onChange={this.onLabelChange} placeholder="Что еще нужно сделать" value={this.state.label}/>
+        <button className="btn btn-outline-secondary float-right" type="submit">Добавить</button>
+      </form>
     );
   }
 }
--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 
 export class ItemStatusFilter extends Component {
+  constructor() {
+    super();
+    this.buttons = [
+      {name: 'all', label: 'Все'},
+      {name: 'active', label: 'Актив'},
+      {name: 'done', label: 'Готово'},
+    ]
+  }
+
   render() {
+    const {filter, onFilterChange} = this.props;
+    const buttons = this.buttons.map(({name, label}) => {
+      const isActive = filter == name;
+      const clazz = isActive ? 'btn-info' : 'btn-outline-secondary';
+      return (
+        <button type="button" className={`btn ${clazz}`} key={name} onClick={() => onFilterChange(name)}>{label}</button>
+      );
+    });
     return (
       <div className="btn-group">
-        <button type="button" className="btn btn-info">All</button>
-        <button type="button" className="btn btn-outline-secondary">Active</button>
-        <button type="button" className="btn btn-outline-secondary">Done</button>
+        {buttons}
       </div>
     );
   }
